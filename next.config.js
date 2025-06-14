d069e68ae5e9e942863ee8264c749ead1651ee28
zou-john <john_zou@brown.edu>
2025-06-13 22:36:58 -0700

    GIT ME OUTTTT

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   trailingSlash: true,  // Add trailing slashes to all routes
   basePath: process.env.NODE_ENV === 'production' ? '/zou-john' : '',  // Only add basePath in production
   assetPrefix: process.env.NODE_ENV === 'production' ? '/zou-john/' : '',  // Only add assetPrefix in production
+  distDir: 'out',  // Specify output directory
 }
 
 module.exports = nextConfig;
